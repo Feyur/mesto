75ffa31cc10636f21777ae3aec7630289af7835e
Feyur <Feyur@yandex.ru>
2022-03-19 21:46:01 +0300

    fix: правки js и верстки

--- scripts/script.js
+++ scripts/script.js
@@ -3,27 +3,29 @@ let closeButton = document.querySelector('.popup__close-button');
 let popup = document.querySelector('.popup');
 let form = document.querySelector('.popup__form');
 let formElement = document.querySelector('.popup__submit');
-let nameInput = document.querySelector('.popup__text');
-let jobInput = document.querySelector('.popup__job');
+let nameInput = document.querySelector('[name="name"]');
+let jobInput = document.querySelector('[name="job"]');
 let profileTitle = document.querySelector('.profile__title');
 let profileDescription = document.querySelector('.profile__description');
 
-openButton.addEventListener ('click', function(){
+function popupOpened() {
   popup.classList.add('popup_opened');
   nameInput.value = profileTitle.textContent;
   jobInput.value = profileDescription.textContent;
-});
+}
 
-closeButton.addEventListener ('click', function(){
-popup.classList.remove('popup_opened');
-});
-
-function formSubmitHandler (evt) {
-  evt.preventDefault();
-  profileTitle.textContent = nameInput.value;
-  profileDescription.textContent = jobInput.value;
+function popupClose() {
   popup.classList.remove('popup_opened');
 }
 
-form.addEventListener('submit', formSubmitHandler);
+openButton.addEventListener('click', popupOpened);
+closeButton.addEventListener('click', popupClose);
 
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  profileTitle.textContent = nameInput.value;
+  profileDescription.textContent = jobInput.value;
+  popupClose();
+}
+
+form.addEventListener('submit', formSubmitHandler);
